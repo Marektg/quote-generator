cbbbe57a235e7139f26bd77111cb20a523f04d29
Marek Kalus <marektg2@gmail.com>
2022-07-29 19:59:58 +0200

    add lang polish

--- script.js
+++ script.js
@@ -1,26 +1,70 @@
-let apiQuotes = [];
-
+// let apiQuotesEn = [];
+console.log(localQuotesEn.length);
+console.log(localQuotesPl.length);
+const langPl = document.getElementById('pl');
+const langEn = document.getElementById('en');
 const quoteArrea = document.querySelector("#quote");
 const quoteAuthor = document.querySelector("#author");
 function newQuote() {
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    quoteArrea.innerHTML = quote.text;    
-    quoteAuthor.innerHTML = quote.author;
+    if (langEn.classList.contains('active')) {
+        const quote = localQuotesEn[Math.floor(Math.random() * localQuotesEn.length)];
+        if (quote.text.length > 120) {
+            quoteArrea.classList.add("long-qoute")
+        } else {
+            quoteArrea.classList.remove('long-quote')
+        }
+        quoteArrea.innerHTML = quote.text;
+        quoteAuthor.innerHTML = quote.author;
+        newQuoteBnt.innerHTML = "New Quote";
+    } else {
+        const quote = localQuotesPl[Math.floor(Math.random() * localQuotesPl.length)];
+        if (quote.text.length > 120) {
+            quoteArrea.classList.add("long-qoute")
+        } else {
+            quoteArrea.classList.remove('long-quote')
+        }
+        quoteArrea.innerHTML = quote.text;
+        quoteAuthor.innerHTML = quote.author;
+        newQuoteBnt.innerHTML = "Nowy cytat";
+    }
+    
 }
 
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl)
-        apiQuotes = await response.json();
-        newQuote();    
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiUrl)
+//         apiQuotesEn = await response.json();
+//         console.log(apiQuotesEn.length);
+//         localStorage.setItem("que", JSON.stringify(apiQuotesEn));
+//         newQuote();    
+//     }
+//     catch (error) {
+//         console.log(error.message)
+//     }
+// }
+
+// getQuotes();
+
+const newQuoteBnt = document.getElementById('new');
+// console.log(newQuoteBnt);
+newQuoteBnt.addEventListener('click', newQuote);
+newQuote();
+
+
+
+
+langPl.addEventListener('click', changeActive);
+langEn.addEventListener('click', changeActive);
+
+function changeActive(event) {
+    if (event.currentTarget.id === "pl" && langEn.classList.contains('active')) {
+        langEn.classList.toggle('active');
+        langPl.classList.toggle('active')
     }
-    catch (error) {
-        console.log(error.message)
+    else if (event.currentTarget.id === "en" && langPl.classList.contains('active')) {
+        langEn.classList.toggle('active');
+        langPl.classList.toggle('active')
     }
+    newQuote();
 }
-
-getQuotes();
-
-const newQuoteBnt = document.querySelector("#new");
-newQuoteBnt.addEventListener('click', newQuote());
